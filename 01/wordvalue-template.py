2fcda8e79ded00572d6b151e6c88b7e9dff6cbf1
RAZZ <rpruteanu@gmail.com>
2021-01-04 00:48:53 +0200

    sdfdsf

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -1,18 +1,36 @@
 from data import DICTIONARY, LETTER_SCORES
+import os
 
 def load_words():
     """Load dictionary into a list and return list"""
-    pass
+    lines_list = []
+    dict_path = os.path.join(os.path.curdir, DICTIONARY)
+    with open(dict_path, 'r') as f:
+        lines_list = f.read().splitlines()
+    return lines_list
 
-def calc_word_value():
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    return sum(LETTER_SCORES[c.upper()] if c.isalpha() else 0 for c in word)
 
-def max_word_value():
+def max_word_value(list_words = None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    if list_words is None:
+        list_words = load_words()
+    max_word = None
+    max_val = 0
+    for word in list_words:
+        cur_val = calc_word_value(word)
+        if cur_val > max_val:
+            max_val = cur_val
+            max_word = word
+    return  max_word, max_val
+
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+    # print(max_word_value(['a', 'bb', 'zyx', 'xyz', 'dfgg']))
+    # print(load_words())
+    # print(calc_word_value(''))
+    # pass # run unittests to validate
